34405a1d8a5e0edd6a3763f53b7da29c3b125609
David Patrick Donohue <donohue76@hotmail.com>
2017-10-05 18:31:57 -0700

    updated _post partial so the post creators name is a link to their eventual profile, added route for the link and created profiles controller with show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -7,4 +9,8 @@ Rails.application.routes.draw do
   end
   
   root 'posts#index'
+  # using GET rather than POST or PATCH because it lets us use a dynamic parameter for the sake of our route. just writing GET 'user_name', our route would be fixed, this setup allows us to pass user names as the params.
+  # we will get the details for what to do with this route from the 'profiles' controller and the 'show' action.
+  # as: :profile lets us use rails routing helpers. We can use the profile_path helper
+  get ':user_name', to: 'profiles#show', as: :profile
 end
