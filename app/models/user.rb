23fde657b544f04483cf0cbd586a62f31a1ceab5
David Patrick Donohue <donohue76@hotmail.com>
2017-10-18 19:41:12 -0700

    completed liking posts functionality, the like heart turns solid red upon clicking on it, if a post has less than or equal to 8 likers, their names will be listed, if there are more, just the number of likers will be displayed, the user name is automatically added to the list of likers  or the 'liked by' number increases by one when liked.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-  validates :user_name, presence:true, length: { minimum: 4, maximum: 16 }
-  has_attached_file :avatar, styles: { medium: '200x200#' }
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
-  has_many :posts, dependent: :destroy
-  has_many :commments, dependent: :destroy
+  acts_as_voter
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  
 end
